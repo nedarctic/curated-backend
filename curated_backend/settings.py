d96406b5a9cf01304a3aa708ed820e304d0bf8c9
justkimtai <justkimtai@gmail.com>
2025-11-24 15:15:50 +0300

    configure cors for local frontend dev

--- curated_backend/settings.py
+++ curated_backend/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+]
