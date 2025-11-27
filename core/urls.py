4456919535d2a42b9fe17c305577b276aec87444
justkimtai <justkimtai@gmail.com>
2025-11-27 19:15:27 +0300

    corrected paths in core urls file

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SubscribeView, MessageView, BookingView
 
 urlpatterns = [
-    path('subscribe', SubscribeView.as_view()),
-    path('book', BookingView.as_view()),
-    path('message', MessageView.as_view()),
+    path('subscribe/', SubscribeView.as_view()),
+    path('book/', BookingView.as_view()),
+    path('message/', MessageView.as_view()),
 ]
