4f498ff25e401259ab5d8c88abbd0f766fbd772c
justkimtai <justkimtai@gmail.com>
2025-11-27 20:49:04 +0300

    registered models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking, Message, SubscriptionEmail
+
+admin.site.register(Booking)
+admin.site.register(Message)
+admin.site.register(SubscriptionEmail)
